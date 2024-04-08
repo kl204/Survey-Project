1da2742cd54837e963c632555c1aded2f411ef75
PUO <86181292+kl204@users.noreply.github.com>
2024-04-08 12:16:50 +0900

    cors 설정

--- src/main/java/com/douzone/surveymanagement/common/testConf/WebConfig.java
+++ src/main/java/com/douzone/surveymanagement/common/testConf/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // 모든 출처 허용
+                .allowedOrigins("https://survey-project-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드 지정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키를 포함한 요청 허용
