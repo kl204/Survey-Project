e93d8d10f349225bd613ceab0d9304c43f8b55dd
PUO <86181292+kl204@users.noreply.github.com>
2024-04-08 11:00:18 +0900

    Cross Origin 해결

---
+++ src/main/java/com/douzone/surveymanagement/common/testConf/WebConfig.java
@@ -0,0 +1,18 @@
+package com.douzone.surveymanagement.common.testConf;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://survey-project-frontend.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
